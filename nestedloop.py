b51234b45831ff036b7014e9d7b8d7d8072f4317
Kubra Kalkan <99320090+kubrakalkan1@users.noreply.github.com>
2022-11-14 20:48:07 +0100

    Update nestedloop.py

--- nestedloop.py
+++ nestedloop.py
@@ -17,3 +17,16 @@
 #     for count in range(x_count):
 #         output += "x"
 #     print(output)
+#### 2D LIST ###
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(matrix[0][1]) #2
+matrix[0][1] = 20
+print(matrix[0][1]) #20
+
+for row in matrix:
+    for item in row:
+        print(item)
