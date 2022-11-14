882cca9a0cc8e18dd1c60bbb5799f8f9fbfbd212
Kubra Kalkan <99320090+kubrakalkan1@users.noreply.github.com>
2022-11-14 19:37:48 +0100

    Create nestedloop.py

---
+++ nestedloop.py
@@ -0,0 +1,19 @@
+# prices = [10,20,30]
+# total = 0
+# for price in prices:
+#     total += price
+# print(total)
+# print(f"Total: {total}")
+# ###### NESTED LOOP ###### easliy generate a list of coordinate
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x},{y})")
+######### F shape with loop #########
+# numbers = [5,2,5,2,2]
+# for number in numbers: # 1. way
+#     print(number * "x")
+# for x_count in numbers: # 2. way
+#     output = ''
+#     for count in range(x_count):
+#         output += "x"
+#     print(output)
